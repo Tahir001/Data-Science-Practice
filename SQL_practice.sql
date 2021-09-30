1826ae28bd8716bc352d8df902da46e1115c39cf
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2021-09-30 16:00:15 -0400

    Finished BASIC SQL queries

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -84,6 +84,60 @@ FROM tutorial.billboard_top_100_year_end
 WHERE year = 2013
 AND "group" NOT ILIKE '%a%'
 
+-- ++++++++++++++++++++
+--  Q11
+-- ++++++++++++++++++++
+-- Write a query that returns all rows from 2012, ordered by song title from Z to A. 
+SELECT *
+FROM tutorial.billboard_top_100_year_end
+WHERE year = 2012
+ORDER BY song_name DESC
+
+-- ++++++++++++++++++++
+--  Q12
+-- ++++++++++++++++++++
+-- Write a query that returns all rows from 2010 ordered by rank, with artists ordered alphabetically for each song. 
+SELECT *
+FROM tutorial.billboard_top_100_year_end
+WHERE year = 2010
+ORDER BY year_rank, artist
+
+-- ++++++++++++++++++++
+--  Q13
+-- ++++++++++++++++++++
+-- Write a query that returns songs that ranked between 10 and 20 (inclusive) in 1993, 2003, or 2013. Order the results by year and rank
+SELECT *
+FROM tutorial.billboard_top_100_year_end
+WHERE year_rank BETWEEN 10 AND 20 AND year in (1993, 2003, 2013)
+ORDER BY year, year_rank
+
+-- ++++++++++++++++++++
+--  Q14
+-- ++++++++++++++++++++
+-- Write a query to count the number of non-null rows in the low column. 
+SELECT COUNT(low) -- NOT NULL Values only. 
+  FROM tutorial.aapl_historical_stock_price
+  
+  -- ++++++++++++++++++++
+--  Q15
+-- ++++++++++++++++++++
+-- Write a query that determines counts of every single column. Which column has the most null values? 
+SELECT COUNT(date) AS num_dates, 
+  COUNT(year) AS num_year, 
+  COUNT(month) AS num_month, 
+  COUNT(open) AS num_open, 
+  COUNT(high) AS num_high, 
+  COUNT(low) AS num_low, 
+  COUNT(close) AS num_close, 
+  COUNT(volume) AS num_volume, 
+  COUNT(id) AS num_id
+  FROM tutorial.aapl_historical_stock_price
+
+-- We can see that num_high has the most null values (i.e least amount of rows). 
+
+
+
+
 
 ---------------------------- HEALTH DATABASE ANSWERS ----------------------------
 -- ++++++++++++++++++++
