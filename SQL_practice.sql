c302eff02d80860f91b0d9a9e0e9eec291eaf022
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2021-10-09 22:28:30 -0400

    Update SQL_practice.sql

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -135,7 +135,16 @@ SELECT COUNT(date) AS num_dates,
 
 -- We can see that num_high has the most null values (i.e least amount of rows). 
 
-
+-- CASE WHEN PRACTICE
+-- NORMAL STRUCTURE: CASE WHEN (right after select), colnam = condition, THEN condition END AS new_column
+-- Example (with multiple conditions) 
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 AND weight <= 250 THEN '201-250'
+            WHEN weight > 175 AND weight <= 200 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
 
 
 
